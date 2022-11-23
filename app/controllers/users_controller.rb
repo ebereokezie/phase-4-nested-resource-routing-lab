3d5aaee1a3779ebb3277790e79525e3aaa231591
Ebere Okezie <ebereokezie@gmail.com>
2022-11-22 19:31:57 -0600

    commit tree 8c40ecc9a4521212729e8d6da843aff789006205

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
 
+
   def show
     user = User.find_by(id: params[:id])
+    if user
+
+    render json: user, include: :items
+    else
+    render_not_found_response
+    end
+  end
+
+  def index
+    user = User.all 
     render json: user, include: :items
   end
 
+  private 
+
+  def render_not_found_response
+    render json: {error: "User not found"}, status: :not_found
+  end
 end
